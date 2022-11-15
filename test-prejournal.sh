47c34890092762d5750b4b07d8662ea8b277a8ef
Yvo Brevoort <yvo@muze.nl>
2022-11-15 20:53:53 +0100

    remove modified testnet file on completion

--- test-prejournal.sh
+++ test-prejournal.sh
@@ -9,3 +9,4 @@ curl -d'["alice","alice123"]' http://localhost:8280/v1/register
 curl -d'["23 Sep 2022","stichting","Federated Timesheets", 8, "hard work"]' http://alice:alice123@localhost:8280/v1/worked-hours
 docker exec -it federation-tests_timeld-cli_1 "/usr/local/bin/node" "/timeld/report.mjs"
 curl -H "Authorization: Bearer testnet-supersecret-token" http://localhost:8180/api/trackers/1
+rm testnet.env
